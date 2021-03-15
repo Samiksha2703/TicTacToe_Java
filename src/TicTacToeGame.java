aa2e555ae493986368f9c539b5e2da7b4f81af8d
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-15 09:46:11 +0530

    [Samiksha].ADD code to ask user input

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,18 +1,37 @@
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
     //Method to initialize array elements
-    public static void createBorad() {
-            char [] board = new char[10];
-            for (int index = 1; index < board.length; index++){
-                board[index] = ' ';
-            }
+    public static char[] createBorad() {
+        char[] board = new char[10];
+        for (int index = 1; index < board.length; index++) {
+            board[index] = ' ';
+        }
+        return board;
     }
-    
+
+    //method to take user input
+    public static void userInput() {
+        char computer = 'O';
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the input X/O.");
+        char userInput = sc.next().charAt(0);
+        if (userInput == computer) {
+            computer = 'X';
+        } else if (userInput != computer) {
+
+        } else {
+            System.out.println("Invalid input.");
+        }
+    }
+
     //main method to execute the program
-    public static void main(String [] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game.");
         //calling method to create board
         createBorad();
+        //calling method to take input
+        userInput();
     }
-
 }
