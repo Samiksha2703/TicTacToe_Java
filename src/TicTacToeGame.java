b31629f4f2ed583a089d7be9ccc120dffe2c177d
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-15 09:27:35 +0530

    [Samiksha].UPDATE change non static method to static

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,20 +1,18 @@
 public class TicTacToeGame {
 
     //Method to initialize array elements
-    public char[] createBorad() {
+    public static void createBorad() {
             char [] board = new char[10];
             for (int index = 1; index < board.length; index++){
                 board[index] = ' ';
             }
-            return board;
     }
-
+    
     //main method to execute the program
     public static void main(String [] args){
         System.out.println("Welcome to Tic Tac Toe Game.");
-        //creating object of class
-        TicTacToeGame ticTacToeGame = new TicTacToeGame();
-        ticTacToeGame.createBorad();
+        //calling method to create board
+        createBorad();
     }
 
 }
