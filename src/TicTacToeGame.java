5a74b660947373f59f1c8c309a9b90521fff80ea
Samiksha2703 <shende.samiksha@rediffmail.com>
2021-03-15 10:47:49 +0530

    [Samiksha].ADD code to display board

--- src/TicTacToeGame.java
+++ src/TicTacToeGame.java
@@ -1,11 +1,15 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class TicTacToeGame {
 
+    public static int index;
+    public static char [] board;
+
     //Method to initialize array elements
     public static char[] createBorad() {
         char [] board = new char[10];
-        for (int index = 1; index < board.length; index++) {
+        for (index = 1; index < board.length; index++) {
             board[index] = ' ';
         }
         return board;
@@ -27,12 +31,22 @@ public class TicTacToeGame {
         return userInput;
     }
 
+    //method to display board
+    public static char[] displayBoard() {
+        int initialize = 1;
+        for (index = initialize; index <= board.length - initialize; index++) {
+            System.out.println(board[index] + " | " + board[index + 1] + " | " + board[index + 2]);
+            initialize = initialize + 3;
+        }
+        return board;
+    }
     //main method to execute the program
     public static void main(String[] args) {
         System.out.println("Welcome to Tic Tac Toe Game.");
         //calling method to create board
-        char [] board = createBorad();
+         board = createBorad();
         //calling method to take input
-        userInput();
+        char input = userInput();
+        displayBoard();
     }
 }
